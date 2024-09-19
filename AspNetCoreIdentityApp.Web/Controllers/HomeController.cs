7c85cc227cf1c26e8c37c7236249d5ae8c4f17b0
Görkem Kaya <gorkemkayas@hotmail.com>
2024-09-19 15:06:45 +0300

    User Register page and its action methods completed.

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,9 +10,12 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -21,6 +26,34 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+
+            var identityResult = await _userManager.CreateAsync(new() { UserName = request.UserName, PhoneNumber = request.Phone, Email = request.Email},request.Password);
+
+            if (identityResult.Succeeded)
+            {
+                TempData["SucceedMessage"] = "Membership process completed successfully.";
+                return RedirectToAction(nameof(SignUp));
+            }
+
+            foreach (IdentityError item in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty,item.Description);
+            }
+
+            return View();
+
+            
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel()
+        {
+
+        }
+        public SignUpViewModel(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+
+        [Display(Name = "Username :")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Email :")]
+        public string Email { get; set; }
+
+        [Display(Name = "Phone :")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Password :")]
+        public string Password { get; set; }
+
+        [Display(Name = "Password confirm :")]
+        public string PasswordConfirm { get; set; }
+
+    }
+}
