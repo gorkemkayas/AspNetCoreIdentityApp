a2dfc626cbbd9e5bf6ccbd0f9b6def8414dc8d8c
Görkem Kaya <gorkemkayas@hotmail.com>
2024-09-19 16:53:20 +0300

    UserList page designed.

---
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using AspNetCoreIdentityApp.Web.Areas.Admin.Models;
+using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
+{
+
+    [Area(nameof(Admin))]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await _userManager.Users.ToListAsync();
+
+            var userViewModelList = userList.Select(x => new UserViewModel()
+            {
+                Id = x.Id,
+                Name = x.UserName,
+                Email = x.Email
+            }).ToList();
+
+            return View(userViewModelList);
+        }
+    }
+}
