bb31d6f69eccf4fc60e783d0a9e7880fc91673b5
Görkem Kaya <gorkemkayas@hotmail.com>
2024-09-20 20:28:04 +0300

    Custom Password Validator added.

---
+++ AspNetCoreIdentityApp.Web/CustomValidations/PasswordValidator.cs
@@ -0,0 +1,33 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AspNetCoreIdentityApp.Web.CustomValidations
+{
+    public class PasswordValidator : IPasswordValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user, string? password)
+        {
+            var errors = new List<IdentityError>();
+
+            if(password!.ToLower().Contains(user.UserName!.ToLower()))
+            {
+                errors.Add(new() { Code = "PasswordCannotContainUserName", Description = "Password cannot contain username!"});
+                
+            }
+
+            if (password!.ToLower() == "12345")
+            {
+                errors.Add(new() { Code = "PasswordCannotBeSimple", Description = "Password cannot be simple as you enter!" });
+
+            }
+
+            if (errors.Any())
+            {
+                return Task.FromResult(IdentityResult.Failed(errors.ToArray()));
+            }
+
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
