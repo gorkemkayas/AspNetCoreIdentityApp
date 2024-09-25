e7fffd0d843300d1ba129f8dd6120d8388bafbac
Görkem Kaya <gorkemkayas@hotmail.com>
2024-09-25 17:04:31 +0300

    User profile page designed and user informations transferred with 'UserViewModel'.

--- AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +11,21 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public MemberController(SignInManager<AppUser> signInManager)
+        public MemberController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            
+            var userViewModel = new UserViewModel{ Email = currentUser.Email, PhoneNumber = currentUser.PhoneNumber, UserName = currentUser.UserName};
+
+            return View(userViewModel);
         }
         public async Task Logout()
         {
