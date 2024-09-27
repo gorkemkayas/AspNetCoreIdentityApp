9d8c76ec4c93beba6dd43a18d338b1dd685e0899
Görkem Kaya <gorkemkayas@hotmail.com>
2024-09-27 15:12:58 +0300

    Role listing, updating and deleting pages and actions completed.

--- AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
@@ -3,6 +3,7 @@ using AspNetCoreIdentityApp.Web.Extensions;
 using AspNetCoreIdentityApp.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
 {
@@ -19,9 +20,13 @@ namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var roles = await _roleManager.Roles.Select(x => new RoleViewModel() {
+                Name = x.Name!, 
+                Id = x.Id}).ToListAsync();
+
+            return View(roles);
         }
 
 
@@ -41,7 +46,61 @@ namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
             return View();
             }
 
+            TempData["SucceedMessage"] = $"The role named as '{request.Name} is created successfully!'";
             return RedirectToAction(nameof(RolesController.Index));    
+        }
+
+        public async Task<IActionResult> RoleUpdate(string Id)
+        {
+            var roleToUpdate = await _roleManager.FindByIdAsync(Id);
+
+            if(roleToUpdate == null)
+            {
+                throw new Exception("The role you specified could not be found.");
+            }
+
+            return View(new RoleUpdateViewModel() {Id =roleToUpdate!.Id, Name = roleToUpdate!.Name!});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleUpdate(RoleUpdateViewModel request)
+        {
+
+            var roleToUpdate = await _roleManager.FindByIdAsync(request.Id);
+
+            string oldRole = roleToUpdate!.Name!;
+
+            roleToUpdate!.Name = request.Name;
+
+            var result = await _roleManager.UpdateAsync(roleToUpdate);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelErrorList(result.Errors);
+                return View();
+            }
+
+            TempData["SucceedMessage"] = $"The role named as '{oldRole}' updated to '{roleToUpdate.Name}'";
+            return RedirectToAction(nameof(RolesController.Index));
+        }
+
+        public async Task<IActionResult> RoleDelete(string Id)
+        {
+            var roleToDelete = await _roleManager.FindByIdAsync(Id);
+            if(roleToDelete == null)
+            {
+                throw new Exception("The role you specified could not be found.");
+            }
+
+            var result = await _roleManager.DeleteAsync(roleToDelete);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.Select(x => x.Description).First());
+            }
+
+            TempData["SucceedMessage"] = $"The role named as '{roleToDelete.Name} is deleted successfully!'";
+            return RedirectToAction(nameof(RolesController.Index));
         }
     }
 }
---
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Models/RoleUpdateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.Models
+{
+    public class RoleUpdateViewModel
+    {
+        [Required(ErrorMessage = "The 'Name' field cannot be left blank.")]
+        [Display(Name = "Role Name :")]
+
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+    }
+}
