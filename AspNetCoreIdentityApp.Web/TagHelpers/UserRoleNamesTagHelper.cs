452ffb1d4ae6ecc23c7c2676333dccf382c9bf4d
Görkem Kaya <gorkemkayas@hotmail.com>
2024-09-28 16:34:37 +0300

    User roles are displayed in the user list. A tag helper created for sustain clarity on '.cshtml' file.

---
+++ AspNetCoreIdentityApp.Web/TagHelpers/UserRoleNamesTagHelper.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace AspNetCoreIdentityApp.Web.TagHelpers
+{
+    public class UserRoleNamesTagHelper : TagHelper
+    {
+        public string UserId { get; set; } = null!;
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRoleNamesTagHelper(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var currentUser = await _userManager.FindByIdAsync(UserId) ?? throw new Exception("User not found in TagHelper!");
+            var userRoles = await _userManager.GetRolesAsync(currentUser!);
+
+            var stringBuilder = new StringBuilder();
+            userRoles.ToList().ForEach(x=>
+            {
+                stringBuilder.Append(@$"<span class='badge bg-secondary me-1'>{x}</span>");
+            });
+            if(userRoles.Count()==0)
+            {
+                stringBuilder.Append(@$"<span class='badge bg-danger me-1'>No Role</span>");
+            }
+
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+
+        }
+    }
+}
