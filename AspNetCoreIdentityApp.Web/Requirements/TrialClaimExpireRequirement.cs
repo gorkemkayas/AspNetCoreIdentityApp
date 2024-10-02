00817f4ef712898792fef6f27822efd633a799b3
Görkem Kaya <gorkemkayas@hotmail.com>
2024-10-02 12:11:00 +0300

    Trial Claim requirements defined.

---
+++ AspNetCoreIdentityApp.Web/Requirements/TrialClaimExpireRequirement.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreIdentityApp.Web.Requirements
+{
+    public class TrialClaimExpireRequirement : IAuthorizationRequirement
+    {
+    }
+    public class TrialClaimExpireRequirementHandler : AuthorizationHandler<TrialClaimExpireRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, TrialClaimExpireRequirement requirement)
+        {
+            if(!context.User.HasClaim(x => x.Type == "TrialClaim"))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var trialExpireDate = context.User.FindFirst("TrialClaim");
+
+            if (DateTime.Now > Convert.ToDateTime(trialExpireDate))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
